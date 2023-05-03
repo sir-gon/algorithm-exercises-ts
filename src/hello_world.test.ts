edbe812711d643caf92f6fda0d652feb1d7fdfa4
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-02 23:47:01 -0400

    Prettier + ESLint fixes.

--- src/hello_world.test.ts
+++ src/hello_world.test.ts
@@ -1,7 +1,7 @@
-import { hello } from "../src/hello_world";
+import { hello } from '../src/hello_world';
 
-describe("test hello", () => {
-  it("should return hello world", () => {
-    expect(hello()).toBe("Hello world!");
+describe('test hello', () => {
+  it('should return hello world', () => {
+    expect(hello()).toBe('Hello world!');
   });
 });
