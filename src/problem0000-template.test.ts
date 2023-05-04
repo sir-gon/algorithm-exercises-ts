14947f8d7ddf7f137b5b2ff345e93ca931f362c4
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-04 14:22:18 -0400

    Template for problems added.

---
+++ src/problem0000-template.test.ts
@@ -0,0 +1,27 @@
+/**
+ *
+ * <TITLE>
+ *
+ * https://projecteuler.net/problem=XX
+ *
+ * <DESCRIPTION>
+ *
+ * ////////////////////////////////////////////////////////////////////////////
+ *
+ * Result found:
+ * ////////////////////////////////////////////////////////////////////////////
+ */
+
+import { problem0000template } from './problem0000-template';
+
+describe('problem 00XX', () => {
+  it('problem 00XX solution found', () => {
+    expect.assertions(1);
+
+    const solutionFound = null;
+
+    const calculated = problem0000template();
+
+    expect(calculated).toBe(solutionFound);
+  });
+});
---
+++ src/problem0000-template.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ * <TITLE>
+ *
+ * https://projecteuler.net/problem=XX
+ *
+ * <DESCRIPTION>
+ *
+ * ////////////////////////////////////////////////////////////////////////////
+ *
+ * Result found:
+ * ////////////////////////////////////////////////////////////////////////////
+ */
+
+export function problem0000template() {
+  const result = null;
+
+  return result;
+}
+
+export default { problem0000template };
