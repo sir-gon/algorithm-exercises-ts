735cb9d30dab89a728b59226268d5eb864ad8e78
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-15 19:14:31 -0400

    Problem 0003: new divisors helper (calculate all divisors of a natural number).

---
+++ src/helpers/divisors.test.ts
@@ -0,0 +1,13 @@
+import { divisors } from './divisors';
+
+describe('divisors of a number', () => {
+  it('divisors of a number', () => {
+    expect.assertions(5);
+
+    expect(divisors(1)).toStrictEqual([1]);
+    expect(divisors(2)).toStrictEqual([1, 2]);
+    expect(divisors(4)).toStrictEqual([1, 2, 4]);
+    expect(divisors(9)).toStrictEqual([1, 3, 9]);
+    expect(divisors(16)).toStrictEqual([1, 2, 4, 8, 16]);
+  });
+});
---
+++ src/helpers/divisors.ts
@@ -0,0 +1,42 @@
+import logger from '../logger';
+
+export const divisors = (target: number): number[] => {
+  let top = Math.abs(target);
+  const divs = [];
+
+  divs.push(1);
+
+  if (target === 1) {
+    return divs;
+  }
+
+  logger.debug(`Find divisors of ${target}`);
+
+  // fast divisors finding loop
+  let i = 2;
+  while (i <= top) {
+    if (top > 2 && target % i === 0) {
+      top = target / i;
+
+      divs.push(i);
+      if (i < top) {
+        divs.push(target / i);
+      }
+    }
+    i += 1;
+  }
+
+  divs.push(target);
+
+  logger.debug(`collected divisors {divs}`);
+
+  // sort divisors
+  divs.sort((a, b) => a - b);
+  logger.debug(`sorted divisors {divs}`);
+
+  return divs;
+};
+
+export default {
+  divisors
+};
