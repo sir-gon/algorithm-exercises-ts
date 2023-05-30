94ead5e4c91ef447c89830b12362505e3dd0e3f1
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-30 14:35:54 -0400

    [REFACTOR] comments fixed

--- src/problem0008.ts
+++ src/problem0008.ts
@@ -4,7 +4,7 @@
  * https://projecteuler.net/problem=8
  *
  *
- * The four adjacent digits in the 1000-digit _number that have the greatest
+ * The four adjacent digits in the 1000-digit number that have the greatest
  * product are 9 × 9 × 8 × 9 = 5832.
  *
  * 73167176531330624919225119674426574742355349194934
@@ -28,20 +28,20 @@
  * 05886116467109405077541002256983155200055935729725
  * 71636269561882670428252483600823257530420752963450
  *
- * Find the thirteen adjacent digits in the 1000-digit _number that have
+ * Find the thirteen adjacent digits in the 1000-digit number that have
  * the greatest product. What is the value of this product?
  */
 
 /// ///////////////////////////////////////////////////////////////////////////
 // About solution:
-// A integer _number with 1000 digits in javascript node.js is translated to
-// special type "Infinity", so it can't be operated as _number.
+// A integer number with 1000 digits in javascript node.js is translated to
+// special type "Infinity", so it can't be operated as number.
 //
-// This solution runs through the _number as an array of 1-character
+// This solution runs through the number as an array of 1-character
 // strings that each digit represents.
 //
 // NOTE: In javascript it is not necessary to do type casting
-// to operate _numbers in strings.
+// to operate numbers in strings.
 //
 // Solution found:
 //    The the greatest product of 13 consecutive digits is: 23514624000
