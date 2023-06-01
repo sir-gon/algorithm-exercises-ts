a694dfcc0b2f8b0429050755e9ec9d8029c49feb
Gonzalo Diaz <gonzalo@gon.cl>
2023-06-01 19:52:08 -0400

    Helper: numberToWord. Express a number (below 1000) in words.

---
+++ src/helpers/numberToWord.test.ts
@@ -0,0 +1,33 @@
+import { numberToWord } from './numberToWord';
+
+describe('transform numbers to word representation in british english', () => {
+  it('transform numbers to word, common cases up to 2 digits', () => {
+    expect.assertions(5);
+
+    expect(numberToWord(1)).toBe('one');
+    expect(numberToWord(16)).toBe('sixteen');
+    expect(numberToWord(20)).toBe('twenty');
+    expect(numberToWord(30)).toBe('thirty');
+    expect(numberToWord(64)).toBe('sixty-four');
+  });
+
+  it('transform numbers to word, common cases up to 3 and 4 digits', () => {
+    expect.assertions(5);
+
+    expect(numberToWord(301)).toBe('three hundred and one');
+    expect(numberToWord(348)).toBe('three hundred and forty-eight');
+    expect(numberToWord(500)).toBe('five hundred');
+    expect(numberToWord(500)).toBe('five hundred');
+    expect(numberToWord(1000)).toBe('one thousand');
+  });
+
+  it('transform numbers to word, border cases', () => {
+    expect.assertions(2);
+
+    expect(numberToWord()).toBe('');
+
+    expect(() => {
+      numberToWord(999999);
+    }).toThrow('invalid value');
+  });
+});
