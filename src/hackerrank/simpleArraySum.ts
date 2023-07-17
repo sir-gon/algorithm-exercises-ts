4a1de3bb2d34a51537d35057502820665fdd46f9
Gonzalo Diaz <gonzalo@gon.cl>
2023-07-17 03:13:11 -0400

    [Hacker Rank]: Simple Array Sum solved ✓

--- src/hackerrank/simpleArraySum.ts
+++ src/hackerrank/simpleArraySum.ts
@@ -2,22 +2,48 @@
  * Simple Array Sum
  *
  * https://www.hackerrank.com/challenges/simple-array-sum/problem
+ *
+ * Given an array of integers, find the sum of its elements.
+ * For example, if the array ar = [1, 2, 3], 1 + 2 + 3 = 6, so return 6.
+ *
+ * # Function Description
+ *
+ * Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+ * simpleArraySum has the following parameter(s):
+ * ar: an array of integers
+ *
+ * # Input Format
+ * The first line contains an integer, , denoting the size of the array.
+ * The second line contains  space-separated integers representing the array's elements.
+ *
+ * # Constraints
+ *
+ * 0 < n, ar[i] <= 1000
+ *
+ * # Output Format
+ * Print the sum of the array's elements as a single integer.
+ *
+ * # Sample Input
+ *
+ * ```
+ * 6
+ * 1 2 3 4 10 11
+ * ```
+ *
+ * # Sample Output
+ *
+ * ```
+ * 31
+ * ```
+ *
+ * # Explanation
+ *
+ * We print the sum of the array's elements: 1 + 2 + 3 + 4 + 10 + 11 = 31.
  */
 
 'use strict';
 
-/*
- * Complete the 'simpleArraySum' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY ar as parameter.
- */
-
 export function simpleArraySum(ar: number[]): number {
-  /*
-   * Write your code here.
-   */
-
   let acum = 0;
 
   for (let i = 0; i < ar.length; i++) {
