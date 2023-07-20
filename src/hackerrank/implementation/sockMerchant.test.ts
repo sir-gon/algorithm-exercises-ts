4c6a3ffac4d1aa09a95ce1b2f3f1a4defc5d5d81
Gonzalo Diaz <gonzalo@gon.cl>
2023-07-20 15:08:25 -0400

    [Hacker Rank]: Sales by Match solved ✓

---
+++ src/hackerrank/implementation/sockMerchant.test.ts
@@ -0,0 +1,18 @@
+import logger from '../../logger';
+
+import { sockMerchant } from './sockMerchant';
+
+describe('Sales by Match', () => {
+  it('Sales by Match solution found', () => {
+    expect.assertions(1);
+
+    const input = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+    const solutionFound = 3;
+
+    const calculated = sockMerchant(input.length, input);
+
+    logger.info(`Sales by Match solution found: ${calculated}`);
+
+    expect(calculated).toStrictEqual(solutionFound);
+  });
+});
