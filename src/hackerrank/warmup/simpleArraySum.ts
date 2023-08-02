ce14b03ce744f22aceb8e45c36e5e7272a111543
Gonzalo Diaz <gonzalo@gon.cl>
2023-08-02 14:36:06 -0400

    [REFACTOR] sonarlint fixes

--- src/hackerrank/warmup/simpleArraySum.ts
+++ src/hackerrank/warmup/simpleArraySum.ts
@@ -3,8 +3,8 @@
 export function simpleArraySum(ar: number[]): number {
   let acum = 0;
 
-  for (let i = 0; i < ar.length; i++) {
-    acum += ar[i];
+  for (const num of ar) {
+    acum += num;
   }
 
   return acum;
