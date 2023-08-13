55a114e7d3961a4a5170b16f2590d22f97219a7e
Gonzalo Diaz <gonzalo@gon.cl>
2023-08-13 14:56:20 -0400

    New implementations using BigInt instead of custom "big number" operations.

--- src/projecteuler/problem0016.test.ts
+++ src/projecteuler/problem0016.test.ts
@@ -1,19 +1,24 @@
 import { logger as console } from '../logger';
 
 import { problem0016 } from './problem0016';
+import { problem0016alt } from './problem0016-alt';
 
 describe('problem 0016', () => {
   it('problem 0016 solution found', () => {
-    expect.assertions(1);
+    expect.assertions(2);
 
     const base = '2';
     const exponent = 1000;
     const solutionFound = '1366';
 
-    const calculated = problem0016(base, exponent);
+    let calculated: string;
 
+    calculated = problem0016(base, exponent);
     console.log(`PROBLEM 0016 solution found: ${calculated}`);
+    expect(calculated).toBe(solutionFound);
 
+    calculated = problem0016alt(base, exponent);
+    console.log(`PROBLEM 0016 alternative solution found: ${calculated}`);
     expect(calculated).toBe(solutionFound);
   });
 });
