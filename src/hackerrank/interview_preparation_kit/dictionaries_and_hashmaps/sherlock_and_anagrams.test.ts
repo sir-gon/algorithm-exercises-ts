622499ecfcc652d5c6f840271ec10f09f683b738
Gonzalo Diaz <devel@gon.cl>
2024-07-31 16:31:37 -0400

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Sherlock and Anagrams. Test cases moved to JSON file. New test case added.

--- src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/sherlock_and_anagrams.test.ts
+++ src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/sherlock_and_anagrams.test.ts
@@ -2,51 +2,14 @@ import { describe, expect, it } from '@jest/globals';
 import { logger as console } from '../../../logger';
 
 import { sherlockAndAnagrams } from './sherlock_and_anagrams';
-
-const TEST_CASES = [
-  {
-    title: 'Sample Test Case 0',
-    tests: [
-      {
-        input: 'abba',
-        expected: 4
-      },
-      {
-        input: 'abcd',
-        expected: 0
-      }
-    ]
-  },
-  {
-    title: 'Sample Test Case 1',
-    tests: [
-      {
-        input: 'ifailuhkqq',
-        expected: 3
-      },
-      {
-        input: 'kkkk',
-        expected: 10
-      }
-    ]
-  },
-  {
-    title: 'Sample Test Case 1',
-    tests: [
-      {
-        input: 'cdcd',
-        expected: 5
-      }
-    ]
-  }
-];
+import TEST_CASES from './sherlock_and_anagrams.testcases.json';
 
 describe('sherlock_and_anagrams', () => {
   it('sherlockAndAnagrams test cases', () => {
-    expect.assertions(5);
+    expect.assertions(15);
 
     TEST_CASES.forEach((testSet) => {
-      testSet.tests.forEach((test) => {
+      testSet?.tests.forEach((test) => {
         const answer = sherlockAndAnagrams(test.input);
 
         console.debug(
