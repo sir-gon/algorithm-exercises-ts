6f3b8f02247e2f110bf31c577d5561bd0cd3fe7c
Gonzalo Diaz <devel@gon.cl>
2025-02-20 23:57:11 -0300

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Arrays: Left Rotation. New JSON data.

--- src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.test.ts
+++ src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.test.ts
@@ -3,42 +3,25 @@ import { logger as console } from '../../../logger';
 
 import arrayLeftRotation from './ctci_array_left_rotation';
 
-import ROT_LEFT_ONE_TEST_CASES from './ctci_array_left_rotation.testcases.json';
+import ROT_LEFT_TEST_CASES from './ctci_array_left_rotation.testcases.json';
 
 describe('ctci_array_left_rotation', () => {
-  it('rotLeftOne Test Cases', () => {
-    expect.assertions(5);
+  it('rotLeft Test cases', () => {
+    expect.assertions(8);
 
-    ROT_LEFT_ONE_TEST_CASES.forEach((test) => {
-      const input = test.numbers;
-      const answer = arrayLeftRotation.rotLeftOne(input);
+    ROT_LEFT_TEST_CASES.forEach((test) => {
+      const numbers = test.input as number[];
+
+      const answer = arrayLeftRotation.rotLeft(
+        numbers,
+        Number(test.d_rotations)
+      );
 
       console.debug(
-        `rotLeftOne(${test.numbers.toString()}) solution found: ${answer.toString()}`
+        `rotLeft(${numbers.toString()}) solution found: ${test.expected.toString()}`
       );
 
       expect(answer).toStrictEqual(test.expected);
-    });
-  });
-
-  it('rotLeft Test cases', () => {
-    expect.assertions(1);
-
-    const ROT_LEFT_TEST_CASES = [
-      { numbers: [1, 2, 3, 4, 5], d_rotations: 4, expected: [5, 1, 2, 3, 4] }
-    ];
-
-    ROT_LEFT_TEST_CASES.forEach((value) => {
-      const answer = arrayLeftRotation.rotLeft(
-        value.numbers,
-        value.d_rotations
-      );
-
-      console.debug(
-        `rotLeft(${value.numbers.toString()}) solution found: ${answer.toString()}`
-      );
-
-      expect(answer).toStrictEqual(value.expected);
     });
   });
 });
